a8bdd6fe2ddabe4077cc14b83737a5490440ff78
Yan Leiyu <2955271992qq.com>
2022-10-27 21:32:22 +0800

    检测子弹与外星人的碰撞

--- alien_invasion.py
+++ alien_invasion.py
@@ -99,6 +99,13 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        # 检查是否有子弹击中了外星人
+        # 如果是，就删除相应的子弹和外星人
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True
+        )
+
+
 
     # 得到一群外星人的坐标
     def _create_fleet(self):
